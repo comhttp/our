064fe76f8c65e997dfdec47a21220aff66134c81
marcetin <marcetin@gmail.com>
2021-11-15 22:08:20 +0100

    da

--- app/our.go
+++ app/our.go
@@ -12,7 +12,6 @@ import (
 	"github.com/comhttp/jorm/mod/coin"
 	"github.com/comhttp/jorm/mod/exchange"
 	"github.com/comhttp/jorm/mod/explorer"
-	"github.com/comhttp/jorm/mod/nodes"
 	"github.com/comhttp/jorm/pkg/cfg"
 	"github.com/comhttp/jorm/pkg/jdb"
 	"github.com/comhttp/jorm/pkg/strapi"
@@ -41,55 +40,10 @@ type (
 	}
 )
 
-func (o *OUR) setExplorers() {
-	c, _ := cfg.NewCFG(o.config.Path, nil)
-	bitNodesCfg, err := c.ReadAll("nodes")
-	utl.ErrorLog(err)
-	o.Explorers = make(map[string]*explorer.Explorer)
-	explorerJDBS := make(map[string]*jdb.JDB)
-	for coin, _ := range bitNodesCfg {
-		//coins[coin] = j.JDBclient(coin)
-		jdbCl, err := o.JDBclient(coin)
-		if err != nil {
-			utl.ErrorLog(err)
-		} else {
-			explorerJDBS[coin] = jdbCl
-			o.NodeCoins = append(o.NodeCoins, coin)
-			coinBitNodes := nodes.BitNodes{}
-			err = c.Read("nodes", coin, &coinBitNodes)
-			utl.ErrorLog(err)
-			eq := explorer.Queries(explorerJDBS, "info")
-			o.Explorers[coin] = eq.NewExplorer(coin)
-			o.Explorers[coin].BitNodes = coinBitNodes
-		}
-	}
-	//eq := explorer.Queries(coins, "info")
-	jdbCl, err := o.JDBclient("coins")
-	if err != nil {
-		utl.ErrorLog(err)
-	} else {
-		cq := coin.Queries(jdbCl, "info")
-		cq.WriteInfo("nodecoins", &coin.Coins{
-			N: len(o.NodeCoins),
-			C: o.NodeCoins,
-		})
-	}
-	return
-}
 func (o *OUR) JDBclient(jdbId string) (*jdb.JDB, error) {
 	return jdb.NewJDB(o.jdbServers[jdbId])
 }
 
-// func (j *JORM) STRAPIhandler() http.Handler {
-// 	r := mux.NewRouter()
-// 	r.StrictSlash(true)
-// 	r.Headers()
-// 	n := r.PathPrefix("/n").Subrouter()
-// 	//n.HandleFunc("/{coin}/nodes", cq.CoinNodesHandler).Methods("GET")
-// 	n.HandleFunc("/{coin}/{nodeip}", cq.nodeHandler).Methods("GET")
-
-// 	return handlers.CORS()(handlers.CompressHandler(utl.InterceptHandler(r, utl.DefaultErrorHandler)))
-// }
 func (o *OUR) ENSOhandlers() http.Handler {
 	//coinsCollection := Queries(j.B["coins"],"coin")
 	c, err := o.JDBclient("coins")
@@ -163,20 +117,16 @@ func (o *OUR) OURhandlers() http.Handler {
 }
 
 func (o *OUR) OurSRV() {
-
 	fmt.Println("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc: ")
 	fmt.Println("Start OUR")
 	fmt.Println("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc: ")
 	fmt.Println("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc: ")
-	// coins := coin.GetCoins(s)
 	fmt.Println("Start OUR process")
 
 	s := strapi.New(o.config.Strapi)
 
 	c, err := o.JDBclient("coins")
 	utl.ErrorLog(err)
-
-	// go func() {
 	fmt.Println("Start logos import")
 
 	// logos := s.GetAll("logos")
@@ -198,42 +148,10 @@ func (o *OUR) OurSRV() {
 
 	// fmt.Println("logoslogoslogoslogoslogoslogoslogoslogoslogoslogos:", logos)
 
-	// for i, cc := range coins {
-	// 	fmt.Println("coinscoinscoinscoinscoinscoinscoinscoinscoinscoinscoins:", cc)
-	// 	fmt.Println("coiiiii:", i)
-	// }
-
 	cq := coin.Queries(c, "coin")
 
 	cq.ProcessCoins(s)
 
-	// // cq := &coin.CoinsQueries{}
-	// jdbCl, err := j.JDBclient("coins")
-	// if err != nil {
-	// 	utl.ErrorLog(err)
-	// }
-	// fmt.Println("jdbCljdbCljdbCljdbCljdbCljdbCl: ", jdbCl)
-	// cq := coin.Queries(jdbCl, "coins")
-	// if cq != nil {
-	// 	cq.ProcessCoins(coins)
-	// }
-
-	// fmt.Println("cqcqcqcqcqcqcq: ", cq.GetAllCoins())
-
-	// ticker := time.NewTicker(999 * time.Second)
-	// quit := make(chan struct{})
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-ticker.C:
-	// 			j.Tickers()
-	// 			log.Print("OKNO wooikos")
-	// 		case <-quit:
-	// 			ticker.Stop()
-	// 			return
-	// 		}
-	// 	}
-	// }()
 	fmt.Println("End OUR process")
 
 	log.Fatal().Err(o.WWW.ListenAndServe())
--- go.mod
+++ go.mod
@@ -3,23 +3,5 @@ module github.com/comhttp/our
 go 1.16
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/comhttp/jdb v0.0.0-20210714214118-31fffcade923 // indirect
-	github.com/comhttp/jorm v0.0.0-20211115195522-c247097f1086
-	github.com/dgraph-io/badger/v3 v3.2103.2 // indirect
-	github.com/felixge/httpsnoop v1.0.2 // indirect
-	github.com/golang/glog v1.0.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/flatbuffers v2.0.0+incompatible // indirect
-	github.com/gorilla/handlers v1.5.1
-	github.com/gorilla/mux v1.8.0
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/rs/zerolog v1.26.0
-	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211113001501-0c823b97ae02 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+
 )
